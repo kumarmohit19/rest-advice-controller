a1a96696746576d8ffca0ecf6a87d8227f86e3b3
Kumar <mohitnevatru@gmail.com>
2021-12-07 01:33:11 +0530

    solution

--- src/main/java/com/hackerrank/restcontrolleradvice/controller/FizzBuzzController.java
+++ src/main/java/com/hackerrank/restcontrolleradvice/controller/FizzBuzzController.java
@@ -5,6 +5,7 @@ import com.hackerrank.restcontrolleradvice.dto.FizzBuzzException;
 import com.hackerrank.restcontrolleradvice.dto.FizzBuzzResponse;
 import com.hackerrank.restcontrolleradvice.dto.FizzException;
 import com.hackerrank.restcontrolleradvice.enums.FizzBuzzEnum;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,17 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FizzBuzzController {
 
-  @RequestMapping(value = "/controller_advice/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<FizzBuzzResponse> getFizzBuzzResponse(@PathVariable("code") String code)
-          throws FizzException, BuzzException, FizzBuzzException {
-    if (FizzBuzzEnum.FIZZ.getValue().equals(code)) {
-      //throw new FizzException
-    } else if (FizzBuzzEnum.BUZZ.getValue().equals(code)) {
-      //throw new BuzzException
-    } else if (FizzBuzzEnum.FIZZBUZZ.getValue().equals(code)) {
-      //throw new FizzBuzzException
-    }
-    //return FizzBuzzResponse
-    return null;
-  }
+	@RequestMapping(value = "/controller_advice/{code}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<FizzBuzzResponse> getFizzBuzzResponse(@PathVariable("code") String code)
+			throws FizzException, BuzzException, FizzBuzzException {
+		if (FizzBuzzEnum.FIZZ.getValue().equals(code)) {
+			throw new FizzException("Fizz Exception has been thrown", "Internal Server Error");
+		} else if (FizzBuzzEnum.BUZZ.getValue().equals(code)) {
+			throw new BuzzException("Buzz Exception has been thrown", "Bad Request");
+		} else if (FizzBuzzEnum.FIZZBUZZ.getValue().equals(code)) {
+			throw new FizzBuzzException("FizzBuzz Exception has been thrown", "Insufficient Storage");
+		}
+		FizzBuzzResponse res = new FizzBuzzResponse("Successfully completed fizzbuzz test", 200);
+
+		return ResponseEntity.ok().body(res);
+	}
 }
--- src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
+++ src/main/java/com/hackerrank/restcontrolleradvice/exception/FizzBuzzExceptionHandler.java
@@ -1,10 +1,37 @@
 package com.hackerrank.restcontrolleradvice.exception;
 
+import com.hackerrank.restcontrolleradvice.dto.BuzzException;
+import com.hackerrank.restcontrolleradvice.dto.FizzBuzzException;
+import com.hackerrank.restcontrolleradvice.dto.FizzException;
+import com.hackerrank.restcontrolleradvice.dto.GlobalError;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class FizzBuzzExceptionHandler extends ResponseEntityExceptionHandler {
 
-  //TODO:: implement handler methods for FizzException, BuzzException and FizzBuzzException
+	// TODO:: implement handler methods for FizzException, BuzzException and
+	// FizzBuzzException
+
+	@ExceptionHandler(FizzException.class)
+	public ResponseEntity<Object> handleFizzException(FizzException ex) {
+		GlobalError err = new GlobalError(ex.getMessage(), ex.getErrorReason());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
+	}
+
+	@ExceptionHandler(BuzzException.class)
+	public ResponseEntity<Object> handleBuzzException(BuzzException ex) {
+		GlobalError err = new GlobalError(ex.getMessage(), ex.getErrorReason());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(FizzBuzzException.class)
+	public ResponseEntity<Object> handleFizzBuzzException(FizzBuzzException ex) {
+		GlobalError err = new GlobalError(ex.getMessage(), ex.getErrorReason());
+		return ResponseEntity.status(HttpStatus.INSUFFICIENT_STORAGE).body(err);
+	}
 }
